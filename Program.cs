0f75dc1543e25ea2f6dcf8345504f9b409ed5420
Bruno César Lima Araújo <1561719@sga.pucminas.br>
2025-02-26 21:50:35 -0300

    Meu primeiro commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program {
+    public static void Main (string[] args) {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++) {
+            string entrada = Console.ReadLine();
+            string[] registro = entrada.Split(' ');
+            char caractere = char.Parse(registro[0]);
+            int x = int.Parse(registro[1]);
+        }
+    }
+}
