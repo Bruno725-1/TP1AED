73f03d3722df4369d5fd0e440ae09ada7294b0c7
Bruno César Lima Araújo <bruuno.laraujo@gmail.com>
2025-03-05 16:48:41 -0300

    Exercício do enigma de Numéria resolvido

---
+++ enigmaDeNumeria/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string entrada = Console.ReadLine();
+        string[] numeros = entrada.Split(' ');
+        for (int i = 0; i < numeros.Length; i++) {
+            string numero = numeros[i];
+            int tamanho = numero.Length;
+            char[] invertido = new char[tamanho];
+            for (int j = 0; j < tamanho; j++) {
+                invertido[j] = numero[tamanho - 1 - j];
+            }
+            Console.WriteLine(new string(invertido));
+        }
+    }
+}
