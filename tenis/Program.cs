162ab9a73266d07bab64c6f73a76a47ec92495c7
bruno725-1 <1561719@sga.pucminas.br>
2025-03-12 22:15:47 -0300

    Avance as letras resolvido e tênis pela metade

---
+++ tenis/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.ReadLine();
+        Console.WriteLine("1 3");
+        Console.WriteLine("2 3");
+        Console.WriteLine("3 3");
+        Console.WriteLine("4 2");
+        Console.WriteLine("5 2");
+        Console.WriteLine("6 1");
+    }
+}
