06405fa0555a4c670d9ce378588505a934c407a6
Bruno César Lima Araújo <bruuno.laraujo@gmail.com>
2025-03-20 16:34:24 -0300

    Tênis 100% na classe pública

--- tenis/Program.cs
+++ tenis/Program.cs
@@ -1,64 +1,47 @@
 ﻿using System;
 class Program {
     public static void Main(string[] args) {
-        Console.WriteLine("Tirar esse prompt antes de mandar para o verde");
         int n = int.Parse(Console.ReadLine());
         for (int i = 0; i < n; i++) {
-            int[] vencedores = Resultados();
-            int[] grupos = ClassificarJogadores(vencedores);
-            ImprimirResultados(vencedores, grupos);
+            lerResultados();
         }
     }
-    public static int[] Resultados() {
-        int[] vencedores = new int[15];
+    public static void lerResultados() {
+        int[,] resultados = new int[15, 3];
+        int[] vitorias = new int[101];
+        int menorJogador = 100;
+        int maiorJogador = 1;
         for (int i = 0; i < 15; i++) {
             string[] entrada = Console.ReadLine().Split(' ');
-            vencedores[i] = int.Parse(entrada[2]);
+            int j1 = int.Parse(entrada[0]);
+            int j2 = int.Parse(entrada[1]);
+            int v = int.Parse(entrada[2]);
+            resultados[i, 0] = j1;
+            resultados[i, 1] = j2;
+            resultados[i, 2] = v;
+            //contagem das vitórias
+            vitorias[v] = vitorias[v] + 1;
+            //descobre os maiores e menores jogadores para a impressão
+            if (j1 < menorJogador)
+            menorJogador = j1;
+            if (j2 < menorJogador)
+            menorJogador = j2;
+            if (j1 > maiorJogador)
+            maiorJogador = j1;
+            if (j2 > maiorJogador)
+            maiorJogador = j2;
         }
-        return vencedores;
-    }
-    public static int[] ClassificarJogadores(int[] vencedores) {
-        int j1 = 0, j2 = 0, j3 = 0, j4 = 0, j5 = 0, j6 = 0;
-        for (int i = 0; i < vencedores.Length; i++) {
-            switch(vencedores[i]) {
-                case 1:
-                j1++;
-                break;
-                case 2:
-                j2++;
-                break;
-                case 3:
-                j3++;
-                break;
-                case 4:
-                j4++;
-                break;
-                case 5:
-                j5++;
-                break;
-                case 6:
-                j6++;
-                break;
-            }
-        }
-        int[] vetor = new int[6] {
-            j1, j2, j3, j4, j5, j6
-        };
-        for (int i = 0; i < vetor.Length; i++) {
-            if (vetor[i] > 4)
-            vetor[i] = 1;
-            else if (vetor[i] == 3 || vetor[i] == 4)
-            vetor[i] = 2;
-            else if (vetor[i] == 1 || vetor[i] == 2)
-            vetor[i] = 3;
-            else
-            vetor[i] = -1;
-        }
-        return vetor;
-    }
-    public static void ImprimirResultados(int[] vencedores, int[] grupos) {
-        for (int i = 0; i < grupos.Length; i++) {
-            Console.WriteLine($"{vencedores[i]} {grupos[i]}");
+        //imprimir os resultados
+        for (int jogador = menorJogador; jogador <= maiorJogador; jogador++) {
+            int c = vitorias[jogador];
+            int grupo = -1;
+            if (c > 4)
+            grupo = 1;
+            else if (c == 3 || c == 4)
+            grupo = 2;
+            else if (c == 1 || c == 2)
+            grupo = 3;
+            Console.WriteLine(jogador + " " + grupo);
         }
     }
 }
