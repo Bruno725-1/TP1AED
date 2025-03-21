62e137cc981e9816673733f7f711654659ec868e
Bruno César Lima Araújo <bruuno.laraujo@gmail.com>
2025-03-21 10:07:14 -0300

    Avance as letras devidamente adicionado ao repositório

---
+++ avanceAsLetras/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program {
+    public static void Main (string[] args) {
+        int t = int.Parse(Console.ReadLine());
+        for (int i = 0; i < t; i++) {
+            string entrada = Console.ReadLine();
+            string[] strings = entrada.Split(' ');
+            string a = strings[0];
+            string b = strings[1];
+            int operacoes = CalcularOperacoes(a, b);
+            Console.WriteLine(operacoes);
+        }
+    }
+    public static int CalcularOperacoes (string a, string b) {
+        int operacoes = 0;
+        for (int i = 0; i < a.Length; i++) {
+            int distancia = (b[i] - a[i] + 26) % 26;
+            operacoes += distancia;
+        }
+        return operacoes;
+    }
+}
